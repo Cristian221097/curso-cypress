c071b6a01fe983313e6a219dace25a3719a06672
Cristian Amancio <cristian_amancio@bhdleon.com.do>
2024-09-21 11:22:01 -0400

    Creacion place order page

---
+++ cypress/e2e/pages/place-order/place-order.data.js
@@ -0,0 +1,14 @@
+export class PlaceOrderData{
+
+    static get testData(){
+        return{
+            name: 'Test name',
+            country: 'United states',
+            city: 'Boston',
+            creditCard: '4023154698774512',
+            month:12,
+            year:2024,
+        }
+    }
+
+}
